fb8fe29b412d3ef8f11b16b82f0c403847ae18e2
Hoocki <80456760+Hoocki@users.noreply.github.com>
2021-10-31 20:54:07 +0800

    Add files via upload

---
+++ Chess.java
@@ -0,0 +1,28 @@
+package chess;
+
+/**
+ *
+ * @author Romes
+ */
+public class Chess extends Figures {
+
+    
+    public static void main(String[] args) throws Exception{
+        try{
+        
+            Figures pawn = new Figures(3, 2);
+            System.out.println(pawn.getFigures());        
+            pawn.setX(4);
+            pawn.setY(6);
+            System.out.println(pawn.toString());
+        }
+        catch(IllegalArgumentException e) {
+                System.err.println("Неверный ввод данных");                
+            }
+        catch(MyException e) {
+                System.err.println("Введённые параметры не соответсвуют диапазону");                
+            }
+        
+    }
+    
+}
